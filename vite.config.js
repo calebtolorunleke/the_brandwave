cc45f19d5613dc70b2feaef844c17681da3954ab
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-07-19 17:31:41 -0700

    change on header background

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,3 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()], base: "./",
-})
-
-  /* global.css or Tailwind CSS file */
-  .scrollbar - hide:: -webkit - scrollbar {
-  display: none;
-}
-.scrollbar - hide {
-  -ms - overflow - style: none;
-  scrollbar - width: none;
-}
